7f92c0500ffbd20b4ea8790aaf6a8006084ec83f
dojinbyeon0823 <dojinbyeon0823@gmail.com>
2024-04-06 10:31:00 +0900

    problem going to 운동종목 설문

--- pages/나이 설문.py
+++ pages/나이 설문.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title("당신의 나이는?")
 
-st.selectbox("나이",["만13세~만14세", "만14세~만15세", "만15세~만16세", "만17세~만18세", "만18세~만 19세"])
+st.number_input("당신의 나이를 입력하세요",value=None, placeholder="나이를 입력하세요...")
 
 next_button = st.button("다음 페이지")
 
 def go_to_운동종목설문():
     st.switch_page("pages/운동종목 설문.py")
 if next_button:
-    go_to_운동종목설문()
+    go_to_운동종목()
