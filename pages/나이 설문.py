3c1764b1f5ab2426cdb360a869de02d5aa810d58
dojinbyeon0823 <dojinbyeon0823@gmail.com>
2024-04-06 11:14:09 +0900

    add st.number and text, table for 매인 화면

--- pages/나이 설문.py
+++ pages/나이 설문.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
 st.title("당신의 나이는?")
-
 st.number_input("당신의 나이를 입력하세요",value=None, placeholder="나이를 입력하세요...")
-
 next_button = st.button("다음 페이지")
 
 def go_to_운동종목설문():
     st.switch_page("pages/운동종목 설문.py")
 if next_button:
-    go_to_운동종목()
+    go_to_운동종목설문()
--- pages/매인화면(추천, 브리핑).py
+++ pages/매인화면(추천, 브리핑).py
@@ -2,7 +2,15 @@ import streamlit as st
 
 st.title("오늘의 추천")
 st.header("오늘의 운동 브리핑")
+briefing_text = st.write("당신은 13세인 남자이며 오늘은 근력 운동을 하였습니다. ")
+st.write(briefing_text)
+
 st.header("오늘의 운동 추천")
+recommend_text = st.write("당신의 추천 종목은 아래와 같습니다.")
+st.write(recommend_text)
+data = [['근력 운동', '윗몸 말아올리기'], ['유산소 운동', '조깅'], ['기타', '축구']]
+st.table(data) 
+
 prize_button = st.button("보상")
 
 def go_to_보상():
