8f31279cce987d1ed6d79b046a31de33cc4f5807
dojinbyeon0823 <dojinbyeon0823@gmail.com>
2024-04-11 19:59:30 +0900

    updata 보상

--- pages/보상.py
+++ pages/보상.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image 
 
 st.title("오늘의 보상")
-st.write ("오늘 운동을 2시간 하였습니다. 오늘의 보상은 2000원어치의 음류수 쿠폰입니다.")
+st.write ("오늘 근력운동을 2시간 하였습니다. 오늘의 보상은 2000원어치의 음류수 쿠폰입니다.")
 
 쿠폰 = Image.open('pages/스타벅스 쿠폰.jpg')
 st.image(쿠폰)
