c85d60b5bb12e0a3f9b499386aa885add15c53dd
dojinbyeon0823 <dojinbyeon0823@gmail.com>
2024-04-18 22:02:51 +0900

    changed background color of streamlit page(test)

--- 처음 화면.py
+++ 처음 화면.py
@@ -1,9 +1,20 @@
 import streamlit as st
 
-def go_to_survey():
-    st.write("설문 페이지로 이동합니다.")
+def set_background(color):
+  style = f"""
+    <style>
+      body {{
+        background-color: {color};
+      }}
+    </style>
+  """
+  st.markdown(style, unsafe_allow_html=True)
 
-st.title("안녕하세요, TA Health Care에 오신 것을 환영합니다!")
+set_background("#228B22")  
+
+with st.container():
+
+  st.title("안녕하세요, TA Health Care에 오신 것을 환영합니다!")
 
 skip_button = st.button("skip")
 next_button = st.button("설문")
@@ -18,4 +29,3 @@ def go_to_매인화면():
     st.switch_page("pages/매인 화면.py")
 if skip_button:
     go_to_매인화면()
-    
