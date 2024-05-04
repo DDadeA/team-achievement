e38456ff6c2487cd6ad93c09e361b51fbafc27a2
sarah4444444 <164589812+sarah4444444@users.noreply.github.com>
2024-05-04 19:11:47 +0900

    오타 수정

--- pages/매인 화면.py
+++ pages/매인 화면.py
@@ -14,8 +14,8 @@ set_background("#228B22")
 
 with st.container():    
     st.markdown("# :violet[오늘의 추천]")
-    st.markdown("# :violet[당신은 13세이이남자이며 오늘은 근력운동을 하였습니다.]")
     st.markdown("# :violet[오늘의 운동 브리핑]")
+    st.markdown("당신은 13세 남자이며 오늘은 근력운동을 했습니다.")
     st.markdown("# :violet[오늘의 운동 추천]")
     
 data = [['근력 운동', '윗몸 말아올리기'], ['유산소 운동', '조깅'], ['기타', '축구']]
--- pages/보상.py
+++ pages/보상.py
@@ -1,23 +1,8 @@
 import streamlit as st
 from PIL import Image 
 
-import streamlit as st
-
-def set_background(color):
-  style = f"""
-    <style>
-      div.stApp {{
-        background-color: {color};
-      }}
-    </style>
-  """
-  st.markdown(style, unsafe_allow_html=True)
-
-set_background("#228B22")  
-
-with st.container():
-    st.markdown("# :violet[오늘의 보상]") 
-    st.markdown("# :violet[오늘 근력운동을 2시간 하였습니다. 오늘의 보상은 2000원어치의 음류수 쿠폰입니다.]")
+st.title("오늘의 보상")
+st.write ("오늘 근력운동을 2시간 했습니다. 오늘의 보상은 2000원어치의 음료수 쿠폰입니다.")
 
 쿠폰 = Image.open('pages/스타벅스 쿠폰.jpg')
 st.image(쿠폰)
